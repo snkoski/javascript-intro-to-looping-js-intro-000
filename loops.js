fa13acd65571fea5236e89d75946cde5d882ec37
Shawn Koski <shawnnkoski@gmail.com>
2018-03-05 12:08:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,9 @@
 function forLoop(array) {
   for(var i = 0; i < 25; i++) {
-    array[i] = "I am ${i} strange loop";
+    if (i === 0) {
+      array[i] = "I am ${i} strange loop";
+    } else {
+    array[i] = "I am ${i} strange loops";
+    }
   }
 }
