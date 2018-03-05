c6f08200665de80a12f3ba79fd2317e1b5dc6a8d
Shawn Koski <shawnnkoski@gmail.com>
2018-03-05 12:32:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -13,6 +13,13 @@ function whileLoop(number) {
   return 'done';
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+
 function doWhileLoop(array) {
-  
+  do {
+    array.shift();
+  }
+  while (array.length > 0 && maybeTrue);
 }
